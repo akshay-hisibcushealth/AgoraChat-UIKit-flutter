fd8f5be75bc6f29f10c04870e8f7590b29ca7378
Akshay <akshay@hibiscushealth.com>
2024-10-04 01:41:58 +0530

    remove time testing

--- lib/widgets/chat_message_list/chat_message_list_item/chat_message_list_text_item.dart
+++ lib/widgets/chat_message_list/chat_message_list_item/chat_message_list_text_item.dart
@@ -26,16 +26,63 @@ class ChatMessageListTextItem extends ChatMessageListItem {
     bool isLeft = message.direction == MessageDirection.RECEIVE;
     ChatTextMessageBody body = message.body as ChatTextMessageBody;
 
-    Widget content = Text(
-      body.content,
-      style: contentStyle ??
+    Widget content = EmojiText(
+      text: body.content,
+      textStyle: contentStyle ??
           (isLeft
               ? ChatUIKit.of(context)?.theme.receiveTextStyle ??
-              const TextStyle(color: Colors.black)
+                  const TextStyle(color: Colors.black)
               : ChatUIKit.of(context)?.theme.sendTextStyle) ??
           const TextStyle(color: Colors.white),
     );
 
     return getBubbleWidget(content);
+  }
+}
+
+class EmojiText extends StatelessWidget {
+  const EmojiText({
+    Key? key,
+    required this.text,
+    required this.textStyle,
+  }) : super(key: key);
+
+  final String text;
+  final TextStyle textStyle;
+
+  @override
+  Widget build(BuildContext context) {
+    return RichText(
+      text: _buildText(text),
+    );
+  }
+
+  TextSpan _buildText(String text) {
+    final children = <TextSpan>[];
+    final runes = text.runes;
+
+    for (int i = 0; i < runes.length;) {
+      int current = runes.elementAt(i);
+      final isEmoji = current > 255;
+      final shouldBreak = isEmoji ? (x) => x <= 255 : (x) => x > 255;
+
+      final chunk = <int>[];
+      while (!shouldBreak(current)) {
+        chunk.add(current);
+        if (++i >= runes.length) break;
+        current = runes.elementAt(i);
+      }
+
+      children.add(
+        TextSpan(
+          text: String.fromCharCodes(chunk),
+          style: TextStyle(
+            fontFamily: isEmoji ? 'EmojiOne' : null,
+          ),
+        ),
+      );
+    }
+
+    return TextSpan(children: children);
   }
 }
